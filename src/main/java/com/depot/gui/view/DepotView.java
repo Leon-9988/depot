4de2207585e107ec8e40439ddb03b2b2719829af
Leon <2844846067@qq.com>
2024-12-26 16:58:33 +0800

    add_parcel add_customer

--- src/main/java/com/depot/gui/view/DepotView.java
+++ src/main/java/com/depot/gui/view/DepotView.java
@@ -108,15 +108,15 @@ public class DepotView extends JFrame {
         initButton = new JButton("Initialize System");
         processButton = new JButton("Process Next Customer");
         searchButton = new JButton("Search Parcel");
-//        addCustomerButton = new JButton("Add Customer");
-//        addParcelButton = new JButton("Add Parcel");
+        addCustomerButton = new JButton("Add Customer");
+        addParcelButton = new JButton("Add Parcel");
 //        deleteCustomerButton = new JButton("Delete Customer");
         
         buttonPanel.add(initButton);
         buttonPanel.add(processButton);
         buttonPanel.add(searchButton);
-//        buttonPanel.add(addCustomerButton);
-//        buttonPanel.add(addParcelButton);
+        buttonPanel.add(addCustomerButton);
+        buttonPanel.add(addParcelButton);
 //        buttonPanel.add(deleteCustomerButton);
         
         // Create log area
